77d251988e37fccf637a6fd10f50c8965aeb2fb6
hardik <hardikready@gmail.com>
2018-05-07 22:44:57 +0530

    add : erroneous condition handled

--- app/src/test/java/com/moviedb/ui/movie/PresenterImplTest.java
+++ app/src/test/java/com/moviedb/ui/movie/PresenterImplTest.java
@@ -14,6 +14,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.List;
 import java.util.Observable;
 
 import io.reactivex.Flowable;
@@ -21,8 +22,11 @@ import io.reactivex.Scheduler;
 import io.reactivex.android.plugins.RxAndroidPlugins;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
+import okhttp3.internal.http.RealResponseBody;
 import retrofit2.HttpException;
 import retrofit2.Response;
+import retrofit2.Retrofit;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.doAnswer;
@@ -57,9 +61,23 @@ public class PresenterImplTest {
     public void checkValidResponseIsShownOnView(){
         Movie movie = new Movie();
         io.reactivex.Observable<Movie> movieObservable = io.reactivex.Observable.just(movie);
+
         when(connectionManager.isConnected()).thenReturn(true);
         doReturn(movieObservable).when(movieInteractor).searchMovie("John Wick");
+
         presenter.onQueryEntered("John Wick");
+
         verify(view).showMovies(movie);
+    }
+
+    @Test
+    public void checkExceptionIsShownOnUI(){
+        io.reactivex.Observable errorObservable = io.reactivex.Observable.<Movie>error(new RuntimeException());
+
+        doReturn(errorObservable).when(movieInteractor).searchMovie("####");
+
+        presenter.onQueryEntered("####");
+
+        verify(view).showMovies(new Movie());
     }
 }
